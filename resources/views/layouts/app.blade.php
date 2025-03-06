e8b87b3e8abb1edded821a82e8ed20f1e775040a
allan <alanbruno.140@gmail.com>
2025-03-06 00:10:56 -0300

    refactor: altera cor de fundo da aplicação

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,26 +6,26 @@
     <title>@yield('title', 'Essentia Group')</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body>
-<header>
-    @livewireStyles
-    <div class="container d-flex justify-content-center align-items-center mt-3">
-        <img src="https://essentiagroup.global/static/media/ho.a335e8fe9332bf2d7218c9e8a3128627.svg"
-             alt="Imagem Centralizada"
-             class="img-fluid"
-        >
-    </div>
-</header>
-<main>
-    @stack('scripts')
-    @livewireScripts
+<body class="bg-light">
+    <header>
+        @livewireStyles
+        <div class="container d-flex justify-content-center align-items-center mt-3">
+            <img src="https://essentiagroup.global/static/media/ho.a335e8fe9332bf2d7218c9e8a3128627.svg"
+                 alt="Imagem Centralizada"
+                 class="img-fluid"
+            >
+        </div>
+    </header>
+    <main>
+        @stack('scripts')
+        @livewireScripts
 
-    @yield('content')
-</main>
-<footer>
-    <div class="container">
-        <p>Essentia Group &copy; {{ date('Y') }}</p>
-    </div>
-</footer>
+        @yield('content')
+    </main>
+    <footer>
+        <div class="container">
+            <p>Essentia Group &copy; {{ date('Y') }}</p>
+        </div>
+    </footer>
 </body>
 </html>
